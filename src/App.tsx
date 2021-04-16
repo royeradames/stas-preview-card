92ecba5c2b804e30bb13ffc42bd44c670096d1ce
royeradames <royeraadames@gmail.com>
2021-04-16 07:27:05 -0400

    📦add text and html tags

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,30 @@ import React from "react";
 import "./styles/main.scss";
 
 function App() {
-  return <main className="main"></main>;
+  return (
+    <main className="main stats">
+      <h2 className="stats__title">
+        Get <span className="stats__title--highlight">insights</span> that help
+        your business grow.
+      </h2>
+      <p className="stats__message">
+        Discover the benefits of data analytics and make better decisions
+        regarding revenue, customer experience, and overall efficiency
+      </p>
+      .
+      <ul>
+        <li className="stats__stat">
+          <span className="stats_stat--highlight">10k+</span> companies
+        </li>
+        <li className="stats__stat">
+          <span className="stats_stat--highlight">314</span> templates
+        </li>
+        <li className="stats__stat">
+          <span className="stats_stat--highlight">12m+</span> queries
+        </li>
+      </ul>
+    </main>
+  );
 }
 
 export default App;
