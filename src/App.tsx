a0a10255d7c8986b67fa572d4774f4a8e17eb427
royeradames <royeraadames@gmail.com>
2021-04-16 08:04:59 -0400

    📦add header images

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import "./styles/main.scss";
 
+// import images
+import mobileHeader from "./images/image-header-desktop.jpg";
+import desktopHeader from "./images/image-header-mobile.jpg";
+
 function App() {
   return (
     <main className="main stats">
-      <h2 className="stats__title">
-        Get <span className="stats__title--highlight">insights</span> that help
-        your business grow.
-      </h2>
-      <p className="stats__message">
-        Discover the benefits of data analytics and make better decisions
-        regarding revenue, customer experience, and overall efficiency
-      </p>
-      .
+      <picture className="stats_img">
+        <source media="(min-width:1440px)" srcSet={desktopHeader} />
+        <img src={mobileHeader} alt="Ladies working in a open office" />
+      </picture>
+      <div className="stats__content">
+        <h2 className="stats__title">
+          Get <span className="stats__title--highlight">insights</span> that
+          help your business grow.
+        </h2>
+        <p className="stats__message">
+          Discover the benefits of data analytics and make better decisions
+          regarding revenue, customer experience, and overall efficiency
+        </p>
+      </div>
       <ul>
         <li className="stats__stat">
           <span className="stats_stat--highlight">10k+</span> companies
