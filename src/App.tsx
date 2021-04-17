46552298810f4aaf7af366cfc95b47ce25268dc3
royeradames <royeraadames@gmail.com>
2021-04-17 00:56:12 -0400

    📦add more tag to easily style list

--- src/App.tsx
+++ src/App.tsx
@@ -30,19 +30,16 @@ function App() {
       </p>
       <ul className="stats__list-stats">
         <li className="stats__stat">
-          <p>
-            <span className="stats__stat--highlight">10k+</span> companies
-          </p>
+          <p className="stats__stat--highlight">10k+</p>
+          <h2 className="stats__stat-heading">companies</h2>
         </li>
         <li className="stats__stat">
-          <p>
-            <span className="stats__stat--highlight">314</span> templates
-          </p>
+          <p className="stats__stat--highlight">314</p>
+          <h2 className="stats__stat-heading">templates</h2>
         </li>
         <li className="stats__stat">
-          <p>
-            <span className="stats__stat--highlight">12m+</span> queries
-          </p>
+          <p className="stats__stat--highlight">12m+</p>
+          <h2 className="stats__stat-heading">queries</h2>
         </li>
       </ul>
     </main>
