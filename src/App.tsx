3d46adc32924ea2bbe3501bb0c33f8195b5ae4c9
royeradames <royeraadames@gmail.com>
2021-05-03 21:50:30 -0400

    📦add stats container

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,38 @@ function App() {
   return (
     <main className="main ">
       <article className="stats">
-        <header className="stats__header">
-          <picture className="stats__header-img">
-            <source media="(min-width: 1240px)" srcSet={desktoImg} />
-            <img src={mobileImg} alt="Three girls in a open office" />
-          </picture>
-          <div className="stats__header-filter"></div>
-        </header>
-        <div className="stats__content">
-          <h2 className="stats__title">
-            Get <span className="stats__title--highlight">insights</span> that
-            help your business grow.
-          </h2>
-          <p className="stats__message">
-            Discover the benefits of data analytics and make better decisions
-            regarding revenue, customer experience, and overall efficiency
-          </p>
-          <ul className="stats__list-stats">
-            <li className="stats__stat">
-              <p className="stats__stat--highlight">10k+</p>
-              <h2 className="stats__stat-heading">companies</h2>
-            </li>
-            <li className="stats__stat">
-              <p className="stats__stat--highlight">314</p>
-              <h2 className="stats__stat-heading">templates</h2>
-            </li>
-            <li className="stats__stat stats__queries">
-              <p className="stats__stat--highlight">12m+</p>
-              <h2 className="stats__stat-heading">queries</h2>
-            </li>
-          </ul>
+        <div className="stats__container">
+          <header className="stats__header">
+            <picture className="stats__header-img">
+              <source media="(min-width: 1240px)" srcSet={desktoImg} />
+              <img src={mobileImg} alt="Three girls in a open office" />
+            </picture>
+            <div className="stats__header-filter"></div>
+          </header>
+          <div className="stats__content">
+            <h2 className="stats__title">
+              Get <span className="stats__title--highlight">insights</span> that
+              help your business grow.
+            </h2>
+            <p className="stats__message">
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency
+            </p>
+            <ul className="stats__list-stats">
+              <li className="stats__stat">
+                <p className="stats__stat--highlight">10k+</p>
+                <h2 className="stats__stat-heading">companies</h2>
+              </li>
+              <li className="stats__stat">
+                <p className="stats__stat--highlight">314</p>
+                <h2 className="stats__stat-heading">templates</h2>
+              </li>
+              <li className="stats__stat stats__queries">
+                <p className="stats__stat--highlight">12m+</p>
+                <h2 className="stats__stat-heading">queries</h2>
+              </li>
+            </ul>
+          </div>
         </div>
       </article>
     </main>
